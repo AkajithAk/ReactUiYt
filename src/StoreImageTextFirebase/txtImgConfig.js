60aa353f56a4fb14a31513baa6cdd25c7d2ebf0d
Coding Comics <73754825+AkajithAk@users.noreply.github.com>
2024-11-20 22:38:29 +0530

    Update txtImgConfig.js

--- src/StoreImageTextFirebase/txtImgConfig.js
+++ src/StoreImageTextFirebase/txtImgConfig.js
@@ -3,10 +3,10 @@ import { getStorage } from "firebase/storage"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7monUFuQDgDB25ZQZCGN_jUnyJzNEyQE",
-  authDomain: "txtimage-64f8e.firebaseapp.com",
-  projectId: "txtimage-64f8e",
-  storageBucket: "txtimage-64f8e.appspot.com",
+  apiKey: "AIzjfjsfvjsfvhfvywtrwfwygwE",
+  authDomain: "jhhjhj-64f8e.firebaseapp.com",
+  projectId: "jhhjhj-64f8e",
+  storageBucket: "jhhjhj-64f8e.appspot.com",
   messagingSenderId: "480409360012",
   appId: "1:480409360012:web:0ceccab9e05eb390260da9"
 };
